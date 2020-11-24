c7bfb0d659418cdb0d8e8a68fa1c53ef0be9608e
Victor <siuleungjing@gmail.com>
2020-11-23 21:28:11 -0500

    Add new functionality:

--- dir.c
+++ dir.c
@@ -2,17 +2,53 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
-int main() {
-    DIR * x = opendir("./");
+int main(int argc, char **argv) {
+    char loc[256];
+    DIR * x;
+    if (argc == 1) {
+        printf("File Location: ");
+        if (fgets(loc, sizeof loc, stdin) != NULL) {
+            int len = strlen(loc);
+            if (len > 0 && loc[len - 1] == '\n') {
+                loc[len - 1] = '\0';
+            }
+        }
+        x = opendir(loc);
+    }
+    else {
+        strncpy(loc, argv[1], sizeof loc);
+        int len = strlen(loc);
+        if (len > 0 && loc[len - 1] == '\n') {
+            loc[len - 1] = '\0';
+        }
+        x = opendir(loc);
+    }
+    if (x == NULL) {
+        printf("errno %d: %s %s\n", errno, strerror(errno), loc);
+        return 0;
+    }
     struct dirent *entry;
     entry = readdir(x);
-    printf("Statistics for directory: %s\n", entry -> d_name);
+    printf("Statistics for directory: %s\n", loc);
     unsigned int size = 0;
     while (entry) {
-        struct stat curr;
-        stat(entry -> d_name, &curr);
-        size += curr.st_size;
+        if (entry -> d_type == DT_REG) {
+            char path[256]; 
+            struct stat curr;
+            int len = strlen(loc);
+            if (loc[len - 1] != '/') {
+                loc[len] = '/';
+                loc[len + 1] = '\0';
+                len++;
+            }
+            strcat(loc, entry -> d_name);
+            printf("%s\n", loc);
+            stat(loc, &curr);
+            size += curr.st_size;
+            loc[len] = '\0';
+        }
         entry = readdir(x);
     }
     rewinddir(x);
